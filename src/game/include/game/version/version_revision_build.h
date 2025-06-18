336996445b41fd507495f974a694bb27f8c49a9f
Svetov Leonid <lenzo@list.ru>
2025-06-18 15:38:31 +0300

    License added. Readme supplemented.

--- src/game/include/game/version/version_revision_build.h
+++ src/game/include/game/version/version_revision_build.h
@@ -1,3 +1,3 @@
 // Generated automatically. Do not modify.
-#define PROJECT_VERSION_REVISION 217cd2d
-#define PROJECT_VERSION_BUILD    4948
+#define PROJECT_VERSION_REVISION e06af24
+#define PROJECT_VERSION_BUILD    4950
